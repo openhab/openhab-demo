4e0db109280853740bffe1628eb5eb11ae8a5c2a
Florian Hotze <dev@florianhotze.com>
2025-09-02 22:10:43 +0200

    [demo] Solar: Forward 0 value through power randomness

--- openhab_demo/conf/transform/randomness.js
+++ openhab_demo/conf/transform/randomness.js
@@ -1,6 +1,10 @@
 // randomness.js - JS Script Transformation
 // Returns a random value from the interval [input - scale/2, input + scale/2).
-// Optionally, the returned value can be limited to either min or max or both.
+// Parameters:
+//   - scale (required): the size of the randomness interval
+//   - min (optional): lower bound for the value
+//   - max (optional): upper bound for the value
+//   - forwardZero (optional): forward zero without applying randomness
 // Example usage: JS(randomness.js?scale=100) -> value in [input - 50, input + 50)
 //                JS(randomness.js?scale=100&min=0&max=100) -> value in [input - 50, input + 50) & [0, 100]
 (function(data) {
@@ -11,6 +15,7 @@
     console.warn('scale parameter must be a float')
     return data
   }
+  if (typeof this.forwardZero !== 'undefined' && data.split(' ')[0] === '0') return data
   let rnd = f + ((this.scale / 2) - Math.random() * this.scale)
   const min = parseFloat(this.min)
   const max = parseFloat(this.max)
