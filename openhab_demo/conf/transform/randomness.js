010109da88827499f7c83cdce8493005ad5e7cb5
Florian Hotze <dev@florianhotze.com>
2025-09-02 20:28:08 +0200

    [demo] Solar Power: Limit randomness value to 0

--- openhab_demo/conf/transform/randomness.js
+++ openhab_demo/conf/transform/randomness.js
@@ -1,18 +1,20 @@
 // randomness.js - JS Script Transformation
 // Returns a random value from the interval [input - scale/2, input + scale/2).
-// Example usage: JS(randomness.js?scale=100) -> [input - 50, input + 50)
+// Optionally, the returned value can be limited to either min or max or both.
+// Example usage: JS(randomness.js?scale=100) -> value in [input - 50, input + 50)
+//                JS(randomness.js?scale=100&min=0&max=100) -> value in [input - 50, input + 50) & [0, 100]
 (function(data) {
   const f = parseFloat(data)
   if (isNaN(f)) return data
-  if (typeof this.scale !== 'string') {
-    console.warn('scale parameter must be defined')
-    return data
-  }
   const scale = parseFloat(this.scale)
   if (isNaN(scale)) {
     console.warn('scale parameter must be a float')
     return data
   }
-  const rnd = Math.random()
-  return f + ((this.scale / 2) - rnd * this.scale)
+  let rnd = f + ((this.scale / 2) - Math.random() * this.scale)
+  const min = parseFloat(this.min)
+  const max = parseFloat(this.max)
+  if (!isNaN(min)) rnd = Math.max(rnd, min)
+  if (!isNaN(max)) rnd = Math.min(rnd, max)
+  return rnd
 })(input)
