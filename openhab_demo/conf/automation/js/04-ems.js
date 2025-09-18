ace77b64d0ab6e01b5926ddd0b72552a8bad55ad
Florian Hotze <dev@florianhotze.com>
2025-09-18 22:24:05 +0200

    [demo] 04-ems.js: Add rules to reset values at BoD/BoM & Fix script broken after Item recreation

--- openhab_demo/conf/automation/js/04-ems.js
+++ openhab_demo/conf/automation/js/04-ems.js
@@ -8,29 +8,32 @@ const { items, rules, Quantity, time } = require('openhab')
 const CRON_BEFORE_EOD_PERSIST = '0 59 23 ? * * *'
 const CRON_BEFORE_EOM_PERSIST = '0 58 23 L * * *' // run before EOD to make sure today's value not counted twice
 
+const CRON_AFTER_BOD = '01 00 00 ? * * *';
+const CRON_AFTER_BOM = '01 00 00 1 * * *';
+
 const BATTERY_CAPACITY = Quantity('10 kWh')
 const STEP_SECONDS = 15
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Items
 // ---------------------------------------------------------------------------------------------------------------------
-const solarPower = items.getItem('EMS_Solar_Power')
-const solarProductionDay = items.getItem('Solar_Production_Day')
+const solarPower = () => items.getItem('EMS_Solar_Power')
+const solarProductionDay = () => items.getItem('Solar_Production_Day')
 
-const loadPower = items.getItem('EMS_Load_Power')
-const consumptionDay = items.getItem('EMS_TotalConsumption_Day')
-const consumptionMonth = items.getItem('EMS_TotalConsumption_Month')
-const selfConsumptionDay = items.getItem('EMS_SelfConsumption_Day')
-const selfConsumptionMonth = items.getItem('EMS_SelfConsumption_Month')
+const loadPower = () => items.getItem('EMS_Load_Power')
+const consumptionDay = () => items.getItem('EMS_TotalConsumption_Day')
+const consumptionMonth = () => items.getItem('EMS_TotalConsumption_Month')
+const selfConsumptionDay = () => items.getItem('EMS_SelfConsumption_Day')
+const selfConsumptionMonth = () => items.getItem('EMS_SelfConsumption_Month')
 
-const gridPower = items.getItem('EMS_Grid_Power')
-const gridConsumptionDay = items.getItem('EMS_GridConsumption_Day')
-const gridConsumptionMonth = items.getItem('EMS_GridConsumption_Month')
-const feedInDay = items.getItem('EMS_FeedIn_Day')
-const feedInMonth = items.getItem('EMS_FeedIn_Month')
+const gridPower = () => items.getItem('EMS_Grid_Power')
+const gridConsumptionDay = () => items.getItem('EMS_GridConsumption_Day')
+const gridConsumptionMonth = () => items.getItem('EMS_GridConsumption_Month')
+const feedInDay = () => items.getItem('EMS_FeedIn_Day')
+const feedInMonth = () => items.getItem('EMS_FeedIn_Month')
 
-const batteryPower = items.getItem('EMS_Battery_Power')
-const batterySoC = items.getItem('EMS_Battery_SoC')
+const batteryPower = () => items.getItem('EMS_Battery_Power')
+const batterySoC = () => items.getItem('EMS_Battery_SoC')
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Load Simulation Rule
@@ -42,7 +45,7 @@ rules.when()
     const isDay = time.toZDT().isBetweenTimes(time.toZDT('07:00'), time.toZDT('22:00'))
     const base = isDay && Math.random() < 0.15 ? -2000 : -400
     const random = base + (150 - Math.random() * 300)
-    loadPower.postUpdate(Quantity(random + ' W'))
+    loadPower().postUpdate(Quantity(random + ' W'))
   })
   .build('Load Simulation', '', ['EMS'])
 
@@ -57,9 +60,9 @@ rules.when()
       return Math.round(qty.toUnit(unit).float).toFixed(decimals)
     }
 
-    const solar = solarPower.quantityState ?? Quantity('0 W')
-    const load = loadPower.quantityState.multiply('-1') ?? Quantity('0 W')
-    let socPercent = batterySoC.numericState ?? 50                    // integer %
+    const solar = solarPower().quantityState ?? Quantity('0 W')
+    const load = loadPower().quantityState.multiply('-1') ?? Quantity('0 W')
+    let socPercent = batterySoC().numericState ?? 50                    // integer %
     let socEnergy = BATTERY_CAPACITY.multiply(socPercent / 100) // kWh
 
     console.info(`Battery Simulation - Current: SoC = ${socPercent} %, Stored Energy = ${socEnergy}`)
@@ -102,26 +105,51 @@ rules.when()
     console.info(`Battery Simulation - New: SoC = ${socPercent} %, Battery Power = ${roundToUnit(battP, 'W')} W, Grid Power = ${roundToUnit(gridP, 'W')} W`)
 
     // Update Items
-    batterySoC.postUpdate(socPercent) // integer %
-    batteryPower.postUpdate(battP)    // +W discharging, -W charging
-    gridPower.postUpdate(gridP)       // +W import, -W export
+    batterySoC().postUpdate(socPercent) // integer %
+    batteryPower().postUpdate(battP)    // +W discharging, -W charging
+    gridPower().postUpdate(gridP)       // +W import, -W export
   })
   .build('Battery Simulation', '', ['EMS'])
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Reset Values Rules
+// ---------------------------------------------------------------------------------------------------------------------
+rules.when()
+  .cron(CRON_AFTER_BOD)
+  .then(() => {
+    const cmd = '0 kWh'
+    consumptionDay().postUpdate(cmd)
+    selfConsumptionDay().postUpdate(cmd)
+    gridConsumptionDay().postUpdate(cmd)
+    feedInDay().postUpdate(cmd)
+  })
+  .build('Reset daily energy values after begin of day', '', ['EMS'])
+
+rules.when()
+  .cron(CRON_AFTER_BOM)
+  .then(() => {
+    const cmd = '0 kWh'
+    consumptionMonth().postUpdate(cmd)
+    selfConsumptionMonth().postUpdate(cmd)
+    gridConsumptionMonth().postUpdate(cmd)
+    feedInMonth().postUpdate(cmd)
+  })
+  .build('Reset monthly energy values after begin of month', '', ['EMS'])
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Energy Calculation Rules
 // ---------------------------------------------------------------------------------------------------------------------
 /**
  * Calculates the daily amount of energy from the power value using the Riemann Sum.
- * @param {items.Item} power the Item providing the power value
- * @param {items.Item} [plusDay] the Item to send the energy value to, if energy >= 0 kWh
- * @param {items.Item} [minusDay] the Item to send the energy value to, if energy < 0 kWh
+ * @param {() => items.Item} power the Item providing the power value
+ * @param {() => items.Item} [plusDay] the Item to send the energy value to, if energy >= 0 kWh
+ * @param {() => items.Item} [minusDay] the Item to send the energy value to, if energy < 0 kWh
  */
 function createDailyEnergyCalculationRule (power, plusDay, minusDay) {
   let label = 'Calculate '
-  if (plusDay) label += plusDay.label
+  if (plusDay) label += plusDay().label
   if (plusDay && minusDay) label += ' and '
-  if (minusDay) label += minusDay.label
+  if (minusDay) label += minusDay().label
   rules.when()
     .system().startLevel(100)
     .or().cron('0 */15 * * * ? *')
@@ -130,22 +158,22 @@ function createDailyEnergyCalculationRule (power, plusDay, minusDay) {
       const begin = time.toZDT().withHour(0).withMinute(0).withSecond(0)
       const end = time.toZDT()
       // Use RiemannType.MIDPOINT approximation, see https://github.com/openhab/openhab-core/pull/4461#issue-2682710626
-      const energy = power.persistence.riemannSumBetween(begin, end, items.RiemannType.MIDPOINT)?.quantityState
+      const energy = power().persistence.riemannSumBetween(begin, end, items.RiemannType.MIDPOINT)?.quantityState
       if (!energy) return
       if (energy.greaterThanOrEqual('0 kWh')) {
-        if (plusDay) plusDay.postUpdate(energy)
-        if (minusDay && minusDay.isUninitialized) minusDay.postUpdate('0 kWh')
+        if (plusDay) plusDay().postUpdate(energy)
+        if (minusDay && minusDay().isUninitialized) minusDay().postUpdate('0 kWh')
       } else if (minusDay) {
-        if (plusDay && plusDay.isUninitialized) plusDay.postUpdate('0 kWh')
-        minusDay.postUpdate(energy.multiply('-1'))
+        if (plusDay && plusDay().isUninitialized) plusDay().postUpdate('0 kWh')
+        minusDay().postUpdate(energy.multiply('-1'))
       }
     }).build(label, '', ['EMS'])
 }
 
 /**
  * Calculates the monthly amount of energy by summarizing all persisted daily values of this month and today's value.
- * @param {items.Item} daily the Item of the daily energy values
- * @param {items.Item} monthly the Item for the monthly energy values
+ * @param {() => items.Item} daily the Item of the daily energy values
+ * @param {() => items.Item} monthly the Item for the monthly energy values
  */
 function createMonthlyEnergyCalculationRule (daily, monthly) {
   rules.when()
@@ -156,12 +184,12 @@ function createMonthlyEnergyCalculationRule (daily, monthly) {
       const begin = time.toZDT().withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0)
       const end = time.toZDT()
       // Use RiemannType.MIDPOINT approximation, see https://github.com/openhab/openhab-core/pull/4461#issue-2682710626
-      const total = daily.persistence.sumBetween(begin, end, 'influxdb')?.quantityState
-      const today = daily.quantityState
-      if (!total && today) monthly.postUpdate(today)
-      if (total && today) monthly.postUpdate(total.add(today))
+      const total = daily().persistence.sumBetween(begin, end, 'influxdb')?.quantityState
+      const today = daily().quantityState
+      if (!total && today) monthly().postUpdate(today)
+      if (total && today) monthly().postUpdate(total.add(today))
     })
-    .build(`Calculate ${monthly.label}`, '', ['EMS'])
+    .build(`Calculate ${monthly().label}`, '', ['EMS'])
 }
 
 createDailyEnergyCalculationRule(loadPower, undefined, consumptionDay)
@@ -172,10 +200,10 @@ rules.when()
   .or().cron('0 */15 * * * ? *')
   .or().cron(CRON_BEFORE_EOD_PERSIST)
   .then(() => {
-    const production = solarProductionDay.quantityState
-    const feedIn = feedInDay.quantityState
+    const production = solarProductionDay().quantityState
+    const feedIn = feedInDay().quantityState
     const selfConsumption = production.subtract(feedIn)
-    selfConsumptionDay.postUpdate(selfConsumption)
+    selfConsumptionDay().postUpdate(selfConsumption)
   }).build('Calculate Daily Self-Consumption', '', ['EMS'])
 
 createMonthlyEnergyCalculationRule(consumptionDay, consumptionMonth)
