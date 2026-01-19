3d2bd9545025eaf575c870a866acb7454178877d
Florian Hotze <dev@florianhotze.com>
2026-01-20 00:10:18 +0100

    [demo] Reduce the area in which the children position are simulated

--- openhab_demo/conf/automation/js/02-position-states.js
+++ openhab_demo/conf/automation/js/02-position-states.js
@@ -5,8 +5,8 @@ rules.when()
   .or().cron('*/15 */1 * ? * * *')
   .then(() => {
     for (const itemName of ['Amelia_Position', 'Oliver_Position']) {
-        const lat = 52.2 + Math.random() * 0.6
-        const lon = 13.06 + Math.random() * 0.6
+        const lat = 52.2 + Math.random() * 0.3
+        const lon = 13.06 + Math.random() * 0.3
         const pos = [lat, lon].join(',')
         items.getItem(itemName).postUpdate(pos)
     }
